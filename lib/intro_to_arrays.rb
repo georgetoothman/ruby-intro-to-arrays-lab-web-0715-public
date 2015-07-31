c8d2f0c4de6c63e0eb8522dc03e2a404d2caec97
George Toothman <george.toothman@gmail.com>
2015-07-31 08:40:16 -0400

    all done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,47 @@
+def instantiate_new_array 
+  my_new_array = []
+end
+
+instantiate_new_array 
+
+def array_with_two_elements
+  my_two_array = [1, 2]
+end
+
+array_with_two_elements
+
+def first_element(taylor_swift)
+  # taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = taylor_swift[0]
+end
+
+# my_first_element
+
+def third_element(taylor_swift)
+  # taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_third_element = taylor_swift[2]
+end
+
+# my_third_element
+
+def last_element(taylor_swift)
+  # taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_last_element = taylor_swift[3]
+end
+
+# my_last_element
+
+def first_element_with_array_methods(south_east_asia)
+  first_country = south_east_asia[0]
+end
+
+def last_element_with_array_methods(south_east_asia)
+  last_country = south_east_asia[3]
+end
+
+def length_of_array(programming_languages)
+  length = programming_languages.length
+end
+
+
+
